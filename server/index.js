48a4eb1ef6145d571c0493f0c02b9beb9a35ad28
Sarthak Jakhmola <sarthakjakhmola21@gmail.com>
2026-06-05 18:59:52 +0530

    fix cors for production

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 const PORT = process.env.PORT || 4000;
  
-app.use(cors());
+app.use(cors({
+  origin: ['https://task-manager-5e5i.vercel.app', 'http://localhost:3000', 'http://localhost:5173']
+}));
 app.use(express.json());
  
 app.use('/api/tasks', taskRoutes);
